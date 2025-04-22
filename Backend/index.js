8d673206612a9aa639a71583ce81030ea804b047
sahilchauhan11 <151041090+sahilchauhan11@users.noreply.github.com>
2025-04-22 20:49:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // CORS configuration
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
